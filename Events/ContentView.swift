bc4e59ed303be9791497d1adc445bcf4a7f84936
Emmanuel Adesile <emma2adesile@gmail.com>
2020-09-01 17:11:34 +0100

    Initial Commit

---
+++ Events/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Events
+//
+//  Created by Emmanuel Adesile on 01/09/2020.
+//  Copyright © 2020 Emmanuel Adesile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
